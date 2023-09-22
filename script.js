e6a9a70f9a2478e9186247e86362ccb5b5477c09
me <me@gmail.com>
2023-09-22 09:46:13 +0200

    fourth commit: JS player functionality

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEL = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEL = document.getElementById('computerChoice');
+const resultTect = document.getElementById('resultText');
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const allGameIcons = document.querySelectorAll('.far');
 
 const choices = {
     rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -6,5 +19,44 @@ const choices = {
     lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
     spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
   };
-  
-  
+
+
+// Reset all 'selected' icons
+function resetSelected() {
+ allGameIcons.forEach((icon) => {
+    icon.classList.remove('selected');
+ });
+}
+
+// Passing player selection value and styling icons
+function select(playerChoice) {
+    resetSelected();
+    // Add 'selected' styling & playerChoice
+    switch(playerChoice) {
+        case 'rock':
+            playerRock.classList.add('selected');
+            playerChoiceEL.textContent = ' --- Rock';
+            break;
+        case 'paper':
+            playerPaper.classList.add('selected');
+            playerChoiceEL.textContent = ' --- Paper';
+            break;
+        case 'scissors':
+            playerScissors.classList.add('selected');
+            playerChoiceEL.textContent = ' --- Scissors';
+            break;
+        case 'lizard':
+            playerLizard.classList.add('selected');
+            playerChoiceEL.textContent = ' --- Lizard';
+            break;
+        case 'spock':
+            playerSpock.classList.add('selected');
+            playerChoiceEL.textContent = ' --- Spock';
+            break;
+        default:
+            break;
+    } 
+}
+
+// Event Listeners
+// See in the html onclick property!!!
